596720658f5b341130f51e5be4d45a81e89dc616
kris <kris@canway.net>
2019-04-15 09:50:23 +0800

    add mock api

--- sysmanage/views/test.py
+++ sysmanage/views/test.py
@@ -34,3 +34,21 @@ class MockViewSet(ViewSet):
             )
         get_user_model().objects.bulk_create(bulk_list)
         return Response()
+
+    @list_route(methods=['get'], url_path='users')
+    def get_users(self, request, *args, **kwargs):
+        """
+        获取所有用户的API数据(无分页)
+        """
+        count = request.data.get('count', 10)
+        faker = Faker(locale='zh_CN')
+        user_list = []
+        for i in range(count):
+            user_list.append({
+                'id': count+1,
+                'username': faker.user_name(),
+                'chname': faker.name(),
+                'phone': faker.phone_number(),
+                'email': faker.safe_email(),
+            })
+        return Response(user_list)
